d29934e77d3e729aefb780346f829cd025ed115d
Marcin <73904943+marco70-ms@users.noreply.github.com>
2021-03-14 09:37:03 +0100

    Add files via upload

---
+++ mozilla-firefox/DOWNLOAD.sh
@@ -0,0 +1,11 @@
+##
+
+VERSION=86.0.1
+
+cd /tmp
+
+wget https://ftp.mozilla.org/pub/firefox/releases/$VERSION/source/firefox-$VERSION.source.tar.xz
+wget https://nodejs.org/dist/v14.16.0/node-v14.16.0.tar.gz
+wget https://github.com/eqrion/cbindgen/archive/v0.17.0.tar.gz
+
+
