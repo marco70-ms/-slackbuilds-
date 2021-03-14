ed5d10e675d2678529023bc6f90ce23df788e11c
Marcin <73904943+marco70-ms@users.noreply.github.com>
2021-03-14 11:23:00 +0100

    Update DOWNLOAD.sh

--- mozilla-firefox/DOWNLOAD.sh
+++ mozilla-firefox/DOWNLOAD.sh
@@ -6,4 +6,4 @@ cd /tmp
 
 wget https://ftp.mozilla.org/pub/firefox/releases/$VERSION/source/firefox-$VERSION.source.tar.xz
 wget https://nodejs.org/dist/v14.16.0/node-v14.16.0.tar.gz
-wget -O cbindgen-0.17.0.tar.gz https://github.com/eqrion/cbindgen/archive/v0.17.0.tar.gz
+wget -O cbindgen-0.18.0.tar.gz https://github.com/eqrion/cbindgen/archive/v0.18.0.tar.gz
